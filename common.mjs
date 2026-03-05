8df1120f1719d3f17d48558c703e4178c60d4ff3
SlimMicheals <slimmicheals77@gmail.com>
2026-03-05 19:15:43 +0100

    test: add dictionary tests and lockfile

--- common.mjs
+++ common.mjs
@@ -1,3 +1,7 @@
 import words from "./words.json" with { type: "json" };
 
 export const getDictionarySize = () => words.length;
+
+export const wordExists = (word) => {
+  return words.includes(word.toLowerCase());
+};
